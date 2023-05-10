4386eb8a8e5d02243db55fd94adc4add6432dd7e
MeghadharsanB <bmeghadharsan@gmail.com>
2023-05-10 16:18:11 +0530

    Initial Commit

--- src/main/java/com/bmd/learnspringboot/service/LoginService.java
+++ src/main/java/com/bmd/learnspringboot/service/LoginService.java
@@ -5,7 +5,6 @@ import com.bmd.learnspringboot.repositories.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
