d4001855efa59875f7568d9f9e1f3968a29c77c5
MeghadharsanB <bmeghadharsan@gmail.com>
2023-05-14 22:17:10 +0530

    ReCommit

--- src/main/java/com/bmd/learnspringboot/service/JwtUtil.java
+++ src/main/java/com/bmd/learnspringboot/service/JwtUtil.java
@@ -1,32 +1,16 @@
 package com.bmd.learnspringboot.service;
 
-<<<<<<< HEAD
-import com.bmd.learnspringboot.controller.LoginRequestBody;
-import com.bmd.learnspringboot.model.Login;
-import com.bmd.learnspringboot.repositories.LoginRepository;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.Data;
-import lombok.val;
-=======
 import com.bmd.learnspringboot.model.RequestBody.LoginRequestBody;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
->>>>>>> a0e8ef36710716f1370aaec428c6b777b4c12a4c
-import org.springframework.beans.factory.annotation.Value;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.*;
-import java.util.function.Function;
+
+import static io.jsonwebtoken.Jwts.builder;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-
-import static io.jsonwebtoken.Jwts.*;
-
-
-
 @Component
 
 
@@ -37,14 +21,8 @@ public class JwtUtil {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
-    private Key getSigningKey() {
-        byte[] keyBytes = SECRET_KEY.getBytes();
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
     private String createToken(Map<String, Object> claims, List<String> subject) {
         long now = System.currentTimeMillis();
-        Date validity = new Date(now); // 1 hour   //set later
         var userToken = builder().setClaims(claims).setSubject(subject.toString()).setIssuedAt(new Date(now))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
         userTokenList.put(subject.get(0),userToken);
