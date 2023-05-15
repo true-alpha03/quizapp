3a6f04356275662a796d17fef59422d2cdd1feb4
Meghadharsan B <83969264+true-alpha03@users.noreply.github.com>
2023-05-16 03:49:21 +0530

    Update TestController.java

--- src/main/java/com/bmd/learnspringboot/TestController.java
+++ src/main/java/com/bmd/learnspringboot/TestController.java
@@ -24,7 +24,7 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<String> fun(){
-        return ResponseEntity.ok("Checking if background works");
+        return ResponseEntity.ok("Final Check");
     }
 
 }
