75bfe007db5d04766c9c9bb29d71506e31b44d01
Meghadharsan B <83969264+true-alpha03@users.noreply.github.com>
2023-05-16 04:04:07 +0530

    Update TestController.java

--- src/main/java/com/bmd/learnspringboot/TestController.java
+++ src/main/java/com/bmd/learnspringboot/TestController.java
@@ -24,7 +24,7 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<String> fun(){
-        return ResponseEntity.ok("If you can see this,then CI/CD works");
+        return ResponseEntity.ok("😀😃😌😏");
     }
 
 }
