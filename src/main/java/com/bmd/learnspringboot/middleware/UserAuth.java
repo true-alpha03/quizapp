503e2f8791bb09f07da788f3382c6ce3871d5971
MeghadharsanB <bmeghadharsan@gmail.com>
2023-05-22 23:03:56 +0530

    Testing on CORS

--- src/main/java/com/bmd/learnspringboot/middleware/UserAuth.java
+++ src/main/java/com/bmd/learnspringboot/middleware/UserAuth.java
@@ -24,7 +24,7 @@ public class UserAuth implements HandlerInterceptor {
         String username = request.getRequestURI().substring(request.getRequestURI().lastIndexOf("/") + 1);
         logger.debug(username);
         logger.debug(jwtUtil.userTokenList.get(username));
-        returnVar = (request.getHeaders("authorization").nextElement().substring(7)).equals(jwtUtil.userTokenList.get(username));
+        returnVar = (request.getHeader("authorization").substring(7)).equals(jwtUtil.userTokenList.get(username));
         return true;
     }
 }
