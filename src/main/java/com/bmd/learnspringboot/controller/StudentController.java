59dea4730b1c96b08cf67d483c7fb5288cc2c93e
MeghadharsanB <bmeghadharsan@gmail.com>
2023-05-23 02:23:09 +0530

    Updated response code

--- src/main/java/com/bmd/learnspringboot/controller/StudentController.java
+++ src/main/java/com/bmd/learnspringboot/controller/StudentController.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/authStudent")
 public class StudentController {
@@ -31,30 +30,35 @@ public class StudentController {
         this.userAuth = userAuth;
         this.quizService = quizService;
     }
-
+    @CrossOrigin(origins = "http://34.125.151.233:3000",
+                allowCredentials = "true",
+                maxAge = 3600,
+            allowedHeaders = {"Authorization", "Content-Type", "Accept", "Cache-Control", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin"}
+    )
     @GetMapping("/getDetails/{username}")
     private ResponseEntity<?> getStudentDetails(@PathVariable final String username) {
         System.out.println(userAuth.returnVar);
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Access-Control-Allow-Origin","*");
-        responseHeaders.set("Access-Control-Allow-Methods","*");
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.set("Access-Control-Allow-Origin","http://34.125.151.233:3000");
+//        responseHeaders.set("Access-Control-Allow-Methods","GET");
+//        responseHeaders.set("withCredentials","true");
         if ((userAuth.returnVar).equals(true)) {
 
-            return ResponseEntity.status(200).headers(responseHeaders).body(loginRepository.getByUsername(username));
+            return ResponseEntity.status(200).body(loginRepository.getByUsername(username));
         }
         else {
 
             HashMap<String,String> resp = new HashMap<>();
             resp.put("message","Unauthorized Access");
-            return  ResponseEntity.status(401).headers(responseHeaders).body(resp);
+            return  ResponseEntity.status(401).body(resp);
         }
     }
 
     @GetMapping("/getQuizzes/{course_id}/{username}")
     private ResponseEntity<?> getQuizzes(@PathVariable final String course_id, @PathVariable final String username){
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Access-Control-Allow-Origin","*");
-        responseHeaders.set("Access-Control-Allow-Methods","*");
+        responseHeaders.set("Access-Control-Allow-Origin","http://34.125.151.233:3000");
+        responseHeaders.set("Access-Control-Allow-Methods", String.valueOf(true));
         if ((userAuth.returnVar).equals(true)) {
             List<Quiz> quizList = quizService.getByCourse_id(course_id);
             if(quizList.size()>0){
